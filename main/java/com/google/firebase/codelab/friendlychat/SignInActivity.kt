2f17e34e38b588714ae129842e823053df9b1b6d
Vraj Patel <60700833+vpatel79@users.noreply.github.com>
2021-07-08 14:36:34 -0400

    Add files via upload

---
+++ main/java/com/google/firebase/codelab/friendlychat/SignInActivity.kt
@@ -0,0 +1,112 @@
+/**
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.firebase.codelab.friendlychat
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
+import com.google.firebase.codelab.friendlychat.databinding.ActivitySignInBinding
+
+class SignInActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySignInBinding
+
+    // ActivityResultLauncher
+    // TODO: Implement
+    private val signIn: ActivityResultLauncher<Intent> =
+        registerForActivityResult(FirebaseAuthUIActivityResultContract(), this::onSignInResult)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        // ...
+    }
+
+    // Firebase instance variables
+    // TODO: implement
+    // Firebase instance variables
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // This codelab uses View Binding
+        // See: https://developer.android.com/topic/libraries/view-binding
+        binding = ActivitySignInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Initialize FirebaseAuth
+        // TODO: implement
+        auth = Firebase.auth
+    }
+
+    public override fun onStart() {
+        super.onStart()
+
+        // If there is no signed in user, launch FirebaseUI
+        // Otherwise head to MainActivity
+        if (Firebase.auth.currentUser == null) {
+            // Sign in with FirebaseUI, see docs for more details:
+            // https://firebase.google.com/docs/auth/android/firebaseui
+            val signInIntent = AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setLogo(R.mipmap.ic_launcher)
+                .setAvailableProviders(listOf(
+                    AuthUI.IdpConfig.EmailBuilder().build(),
+                    AuthUI.IdpConfig.GoogleBuilder().build(),
+                ))
+                .build()
+
+            signIn.launch(signInIntent)
+        } else {
+            goToMainActivity()
+        }
+    }
+
+    private fun signIn() {
+        // TODO: implement
+    }
+
+    private fun onSignInResult(result: FirebaseAuthUIAuthenticationResult) {
+        if (result.resultCode == RESULT_OK) {
+            Log.d(TAG, "Sign in successful!")
+            goToMainActivity()
+        } else {
+            Toast.makeText(
+                this,
+                "There was an error signing in",
+                Toast.LENGTH_LONG).show()
+
+            val response = result.idpResponse
+            if (response == null) {
+                Log.w(TAG, "Sign in canceled")
+            } else {
+                Log.w(TAG, "Sign in error", response.error)
+            }
+        }
+    }
+
+    private fun goToMainActivity() {
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
+    }
+
+    companion object {
+        private const val TAG = "SignInActivity"
+    }
+}
